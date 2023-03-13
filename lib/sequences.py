26385db2b79efb6d6df262f212e95cdfb31446f7
Saki <sbasken@gmail.com>
2023-03-13 15:35:37 -0700

    complete print_fibonacci function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    result = []
+    for i in range(length):
+        if len(result) == 0:
+            result.append(0)
+        elif len(result) == 1:
+            result.append(1)
+        elif len(result) >= 2:
+            number = result[i-2] + result[i-1]
+            result.append(number)
+    print(result)
